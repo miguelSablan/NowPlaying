41a3f7c8ad401c63a2d90a62eb9c313c24a7d30a
Miguel <msabl005@fiu.edu>
2022-01-28 22:41:12 -0500

    Initial Commit

---
+++ NowPlaying/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NowPlaying
+//
+//  Created by Miguel Sablan on 1/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
