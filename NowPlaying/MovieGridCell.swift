080d1f7cde9d49990211fb2cf339b4fcab204386
MIGUEL <msabl005@fiu.edu>
2022-02-10 23:06:19 -0500

    Flix Part 2 Done

---
+++ NowPlaying/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  NowPlaying
+//
+//  Created by Miguel Sablan on 2/10/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
